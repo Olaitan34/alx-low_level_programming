cb65b52e203d798afcfe499cc25a55dd477e8252
Olaitan34 <emmfatsneh@gmail.com>
2023-08-11 04:45:37 +0100

    c programming language

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Descriotion: This is the C program that printbthe size of various type of computer
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    printf("Size of char: %zu byte(s)\n", sizeof(char));
+    printf("Size of int: %zu byte(s)\n", sizeof(int));
+    printf("Size of long long: %zu byte(s)\n", sizeof(long long));
+    printf("Size of float: %zu by bytes(s)\n", sizeof(float));
+    printf("Size of long: %zu byte(s)\n", sizeof(long));
+    printf("Size of double: %zu byte(s)\n", sizeof(double));
+    printf("Size of long double: %zu byte(s)\n", sizeof(long double));
+
+    return (0);
+
+}
