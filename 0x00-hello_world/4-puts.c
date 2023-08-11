c408a48b4a02d4727d69016fb669838ab73cab50
Olaitan34 <emmfatsneh@gmail.com>
2023-08-11 02:30:22 +0100

    commiting of c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 
+/**
+ * c program to print "Programming is like building a multilingual puzzle"
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
