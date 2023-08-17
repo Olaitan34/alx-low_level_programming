5dbfb3cbf4e92c59d39b233cb867b25ae6b6a504
Olaitan34 <emmfatsneh@gmail.com>
2023-08-17 08:34:46 +0100

    hello world

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,12 +13,15 @@ int main(void)
 
         i = 0;
 
-        while (i < 10)
-        {
-                putchar(i);
-        }
+	/**
+       	* while (i < 10)
+        *
+        *
+	* putchar(i);
+	}
+	*/
 
-        printf("don’t delete it! \\o/\n");
+        printf("Infinite loop avoided!! \\o/\n");
 
         return (0);
 }
