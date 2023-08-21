9ae4d0d43f97e5fe64513b958e9c7adfa1f4e7c7
Olaitan34 <emmfatsneh@gmail.com>
2023-08-21 22:47:01 +0100

    my commit for array and pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,7 +4,7 @@
 /**
  * print_array - function that prints n elements of an array of integers,
  * @a: This is the pointer
- * n: The number of elements of the array to be printed.
+ * @n: The number of elements of the array to be printed.
  */
 
 void print_array(int *a, int n)
