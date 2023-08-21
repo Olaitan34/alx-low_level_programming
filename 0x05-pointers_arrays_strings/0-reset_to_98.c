45f8da09dfb7745dce9d7f84d6fb61cadaa95fe2
Olaitan34 <emmfatsneh@gmail.com>
2023-08-21 15:57:27 +0100

    my pointer and array functions

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * function that takes a pointer to an int as parameter.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;	
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * my header files
+ */
+	
+void _putchar(void);
+void reset_to_98(int *n);
+
+#endif
