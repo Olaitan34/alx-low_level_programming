9abd978d059c557f43281be4787c89ede944e885
Olaitan34 <emmfatsneh@gmail.com>
2023-08-21 20:12:45 +0100

    my pointer and array functions

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Swap_int - swaps the values of two integers,
+ * swap_int - swaps the values of two integers,
  * @a: The first integer integer to swap
  * @b: The second integer to swap.
  */
