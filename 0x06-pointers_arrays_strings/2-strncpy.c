4b089ba5c0592c184a581e55b6d1d90762f40f2a
Olaitan34 <emmfatsneh@gmail.com>
2023-08-24 23:33:14 +0100

    growth process

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,24 @@
-#include "main.h"
-
 /**
- * _strnncpy: to copy string,
- * @i: the declared integer
- * Return: 0
- */
+ * _strncpy - A function that copies a string.
+ *
+ * @dest: pointer to destination input buffer
+ * @src: pointer to source input buffer
+ * @n: bytes of @src
+ *
+ * Return: @dest
+*/
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
-	while (i < n)
-	{	
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = '\0';
 		i++;
-	}	
-	return 0;
+	}
+
+	return (dest);
 }
