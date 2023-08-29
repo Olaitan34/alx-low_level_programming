52a72e896dfd3dd6230b9ce4672937065385de81
Olaitan34 <emmfatsneh@gmail.com>
2023-08-29 03:30:06 +0100

    happy commiting

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,5 +6,7 @@ void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 char *_strstr(char *haystack, char *needle);
-	
+char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
+
 #endif
