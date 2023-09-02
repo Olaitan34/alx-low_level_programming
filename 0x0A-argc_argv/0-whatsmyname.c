e3f7e2c2784c18c31a97bee883db73c090b32bed
Olaitan34 <emmfatsneh@gmail.com>
2023-09-02 02:58:38 +0100

    my actual commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,18 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-    /* Check if at least one argument is provided (the program name) */
-    if (argc >= 1)
-    {
-        /* Print the program name (argv[0]) */
-        printf("%s\n", argv[0]);
-    }
-    else
-    {
-        /* Print an error message if there are no arguments */
-        printf("No program name provided.\n");
-    }
-
-    return (0);
+	if (argc > 0)
+	printf("%s\n", *argv);
+	return (0);
 }
-
