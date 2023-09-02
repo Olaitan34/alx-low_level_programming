d1e2f5c1ce2c97333fa1d92e5317503317040665
Olaitan34 <emmfatsneh@gmail.com>
2023-09-02 03:36:39 +0100

    my commit

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,10 +8,10 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
-	for (i=0; i < c; i++)
-	printf("%s\n" *argv[i]);
+	for (i=0; i < argc; i++)
+	printf("%s\n", argv[i]);
 	return (0);
 }
