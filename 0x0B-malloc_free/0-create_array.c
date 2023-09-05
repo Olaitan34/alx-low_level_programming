61ff6c07d62520d044eff822cb7b9b8f1ab8e1d9
Olaitan34 <emmfatsneh@gmail.com>
2023-09-05 11:17:43 +0100

    malloc function

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i = 0;
+
+	if (size == 0)
+		return (NULL);
+
+	p = (char *) malloc(size * sizeof(char));
+	
+	if (p == NULL)
+		return (0);
+	
+	while (i < size)
+	{
+		*(p + i) = c;
+		i++;
+	}
+
+	*(p + i) = '\0';
+
+	return (p);
+}
