7bb648c514f0a6a088e70ce1d5da6e3901a3fedb
Olaitan34 <emmfatsneh@gmail.com>
2023-09-11 18:20:12 +0100

    my macros

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - entry point.
- * printf: To print the filename.
- * Return: Always 0
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("%s\n", __FILE__);
-
-	return 0;
+	return (0);
 }
