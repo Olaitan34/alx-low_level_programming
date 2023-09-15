c4ed45bed226323591fa9567f2591235b4c12026
Olaitan34 <emmfatsneh@gmail.com>
2023-09-15 16:27:49 +0100

    variadic babies

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Sums a variable number of integers.
+ * @n: The number of integers to sum.
+ * @...: The integers to sum.
+ *
+ * Return: The sum of the integers, or 0 if no integers are provided.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+        unsigned int result = 0;
+	unsigned int i = 0;
+
+        va_list arg;
+        va_start(arg, n);
+
+        for (i = 0; i < n; i++)
+        {
+                result += va_arg(arg, unsigned int);
+        }
+        va_end(arg);
+
+        return (n > 0) ? result : 0; /* Return result if n > 0, otherwise return 0 */
+}
+
