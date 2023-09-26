634568e4a6d8ea83034c4c2e0a289b88380a0851
Olaitan34 <emmfatsneh@gmail.com>
2023-09-26 16:42:15 +0100

    linked list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h> // Include stdlib.h for malloc
 #include "lists.h"
 
 /**
-  * add_nodeint - adds a new node
-  * @head: head node
-  * @n: value to populate it with
-  * Return: returns a new address created lisr
-  */
+ * add_nodeint - adds a new node at the beginning of a listint_t list.
+ * @head: A pointer to the head of the list.
+ * @n: The integer to be added to the new node.
+ * Return: A pointer to the new node, or NULL on failure.
+ */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *ptr = malloc(sizeof(listint_t));
-	
-	if (ptr == NULL)
-		return(NULL);
-	ptr->n = n;
-	ptr->next = *head;
+    listint_t *ptr = malloc(sizeof(listint_t));
 
-	*head = ptr;
-	return(*head);
+    if (ptr == NULL)
+        return (NULL);
+
+    ptr->n = n;
+    ptr->next = *head;
+
+    *head = ptr;
+    return (*head);
 }
