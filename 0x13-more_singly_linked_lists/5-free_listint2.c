ffe009cad24ce349d1fa79136028d7f9f09762e4
Olaitan34 <emmfatsneh@gmail.com>
2023-09-26 19:56:39 +0100

    linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 
 /**
- * free_listint2 - Frees a listint_t list.
- * @head: A pointer to the head of the list.
+ * free_listint2 - Frees a listint_t list
+ *
+ * @head: A pointer to the head of the list
  */
 void free_listint2(listint_t **head)
 {
