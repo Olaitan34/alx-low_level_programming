2b8d824625e56c179973035e77982247dbc0226a
Olaitan34 <emmfatsneh@gmail.com>
2023-09-26 19:51:00 +0100

    linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,17 +7,16 @@
  */
 void free_listint2(listint_t **head)
 {
-    listint_t *ptr;
+	listint_t *ptr;
 
-    if (head == NULL)
-        return;
-    while (*head != NULL)
-    {
-        ptr = *head;
-        *head = (*head)->next;
-        free(ptr);
-    }
-
-    *head = NULL;
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+        	ptr = *head;
+        	*head = (*head)->next;
+        	free(ptr);
+	}
+	*head = NULL;
 }
 
