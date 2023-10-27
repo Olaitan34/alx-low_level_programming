3621e109c5a173adda47c2bc524b55df07752eb6
Olaitan34 <emmfatsneh@gmail.com>
2023-10-27 01:47:01 +0100

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,15 +12,15 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int rtn = 0;
 	if (b == NULL)
 	{
-		return 0;
+		return (0);
 	}
 	for (; *b; b++)
 	{
 		if (*b != '0' && *b != '1')
 		{
-			return 0;
+			return (0);
 		}
 		rtn = (rtn * 2) + (*b - '0');
 	}
-	return rtn;
+	return (rtn);
 }
